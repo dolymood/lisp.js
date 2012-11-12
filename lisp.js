76682d0bc4a3503e08996938949cb17cac2c9799
dianmiao <dianmiao@sohu-inc.com>
2012-11-12 10:50:35 +0800

    replace tab width space

--- lisp.js
+++ lisp.js
@@ -93,12 +93,12 @@ var lisp = (function() {
             },
         // 定义
         'define': function(val, exp) {
-				this.variables[val] = evl(exp, this); 
+                this.variables[val] = evl(exp, this); 
             },
         // 过程（函数）
         'lambda': function(vars, exp) {
                 var func_map = {};
-				func_map.variables = {};
+                func_map.variables = {};
                 func_map._parent_ = this;
                 return function() {
                     var args = arguments;
@@ -126,7 +126,7 @@ var lisp = (function() {
                 return func.apply(null, args);
             }
     };
-	defaultMap.variables = {};//作用域中的定义的变量
+    defaultMap.variables = {};//作用域中的定义的变量
     
     if (!Array.prototype.forEach) {
         Array.prototype.forEach = function(fn, scope) {
@@ -147,7 +147,7 @@ var lisp = (function() {
             return result;
         };
     }
-	
+    
     // '(+ 1 +2 (- 3 5.6) -8.7)'→
     // ["(", "+", "1", "+2", "(", "-", "3", "5.6", ")", "-8.7", ")"]
     function toArray(str) {
@@ -167,15 +167,15 @@ var lisp = (function() {
         var token = strAry.shift(), result = [], reVal;
         if (token == '(') {
             if (strAry.length > 0) {
-			    while (strAry[0] !== ')') {
-					result.push(read_from(strAry));
-				}
-				strAry.shift();
-				return result;//maybe todo:可以加上长度的判断 ["+", 2]这样的可以考虑是不对的
-			} else {
-			    throw '括号中没内容.';
-			}
-			
+                while (strAry[0] !== ')') {
+                    result.push(read_from(strAry));
+                }
+                strAry.shift();
+                return result;//maybe todo:可以加上长度的判断 ["+", 2]这样的可以考虑是不对的
+            } else {
+                throw '括号中没内容.';
+            }
+            
         } else if (token == ')') {
             throw 'unexpected )'
         } else {
@@ -186,8 +186,8 @@ var lisp = (function() {
     // parse value
     function parseVal(val) {
         if (val == 'true') return true;
-		if (val == 'false') return false;
-		var reVal = +val;
+        if (val == 'false') return false;
+        var reVal = +val;
         if (isNaN(reVal)) {//非数字
             reVal = val;
         }
@@ -197,8 +197,8 @@ var lisp = (function() {
     // '(+ 1 +2 (- 3 5.6) -8.7)'→
     // ["+", 1, 2, ["-", 3, 5.6], -8.7]
     function parse(str) {
-		str = str.replace(/[\r\t\n]/g, " ");
-		return read_from(toArray(str));
+        str = str.replace(/[\r\t\n]/g, " ");
+        return read_from(toArray(str));
     }
     
     // 查找包含key的对象
@@ -227,10 +227,10 @@ var lisp = (function() {
         }
         return null;
     }
-	
-	// 查找对象（对象的_parent_）默认key值
-	function findDefault(key, obj) {
-	    var rVal = obj[key];
+    
+    // 查找对象（对象的_parent_）默认key值
+    function findDefault(key, obj) {
+        var rVal = obj[key];
         if (rVal !== undefined) {
             return rVal;
         } else {
@@ -239,7 +239,7 @@ var lisp = (function() {
             }
         }
         return null;
-	}
+    }
     
     // 处理lisp的9种情况
     // 常量 变量 引用 条件 赋值 定义 过程 序列 过程调用
@@ -248,7 +248,7 @@ var lisp = (function() {
             case '[object Array]': //进一步解析
                 var startStr = array[0];
                 var startStrVal = find(startStr, obj);
-				var startStrValD = findDefault(startStr, obj);
+                var startStrValD = findDefault(startStr, obj);
                 if (startStrVal || startStrValD) {
                     if (startStrValD) {//基本函数调用
                         return startStrValD.apply(obj, array.slice(1));
@@ -260,16 +260,16 @@ var lisp = (function() {
                 }
             break;
             case '[object String]': //定义的变量||字符串
-				var returnValue = find(array, obj);//自定义变量
-				if (!returnValue) {//字符串
-				    returnValue = array;
-				}
+                var returnValue = find(array, obj);//自定义变量
+                if (!returnValue) {//字符串
+                    returnValue = array;
+                }
                 return returnValue;
                 break;
-			case '[object Number]': //常量数字
-			case '[object Undefined]': //undefined
-			case '[object Null]': //null
-			case '[object Boolean]': //boolean
+            case '[object Number]': //常量数字
+            case '[object Undefined]': //undefined
+            case '[object Null]': //null
+            case '[object Boolean]': //boolean
                 return array;
                 break;
             default:
